19d4c59fcdc5507743824495c56bf40d2ec00138
twepi <228lol322kek@gmail.com>
2023-05-25 22:47:55 +0300

    db name changed

--- src/mongo/storage/connect.ts
+++ src/mongo/storage/connect.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const url = "mongodb://localhost:27017/BlogWebsite"
+const url = "mongodb://localhost:27017/testWork"
 
 export const mongoConnect = async () => {
     try {
